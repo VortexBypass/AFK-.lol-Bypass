45bc25dd4fa821352ad4e3de26013048901560b4
VortexBypass <taykay1056@gmail.com>
2025-10-31 20:40:25 -0300

    Update AFKdotlol.meta.js

--- AFKdotlol.meta.js
+++ AFKdotlol.meta.js
@@ -1,17 +1,19 @@
 // ==UserScript==
-// @name         AFK™.lol  Bypass
+// @name         AFK™.lol Bypass
 // @namespace    https://discord.gg/sideloading
-// @version      1.1.3
-// @description  Bypass rekonise bstlar and mboost.me
+// @version      1.1.4
+// @description  Bypass rekonise bstlar and mboost.me with Volcano/Work.ink support
 // @author       AFK™.lol
 // @match        https://bstlar.com/*
 // @match        https://rekonise.com/*
 // @match        https://mboost.me/*
 // @match        https://linkvertise.com/*
 // @match        https://link-unlock.com/*
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
 // @icon         https://i.ibb.co/KcXkWgGm/B57-FBD3-E-489-E-4-F0-D-A5-C0-08017-DA44-C4-E.png
-// @grant        none
-// @run-at       document-end
+// @grant        unsafeWindow
+// @run-at       document-start
 // @require      https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/main/AFKdotlol.yes.js
 // @downloadURL  https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/main/AFKdotlol.user.js
 // @updateURL    https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/main/AFKdotlol.meta.js
