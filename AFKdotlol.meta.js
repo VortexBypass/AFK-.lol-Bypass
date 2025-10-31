42f16282bc34f32a529a16e7aff89aac7f3fbdf0
VortexBypass <taykay1056@gmail.com>
2025-10-31 15:36:01 -0300

    Update AFKdotlol.meta.js

--- AFKdotlol.meta.js
+++ AFKdotlol.meta.js
@@ -1,20 +1,18 @@
 // ==UserScript==
 // @name         AFK™.lol  Bypass
 // @namespace    https://discord.gg/sideloading
-// @version      1.0.7
-// @description  Bypass rekonise, bstlar, mboost.me, Linkvertise and Link-Unlock using multiple backends (TRW API, direct endpoints).
+// @version      1.1.3
+// @description  Bypass rekonise bstlar and mboost.me
 // @author       AFK™.lol
 // @match        https://bstlar.com/*
 // @match        https://rekonise.com/*
 // @match        https://mboost.me/*
-// @match        *://*linkvertise*/*
-// @match        *://*link-unlock*/*
+// @match        https://linkvertise.com/*
+// @match        https://link-unlock.com/*
 // @icon         https://i.ibb.co/KcXkWgGm/B57-FBD3-E-489-E-4-F0-D-A5-C0-08017-DA44-C4-E.png
 // @grant        none
 // @run-at       document-end
-// @homepageURL  https://github.com/VortexBypass/AFK-.lol-Bypass
-// @supportURL   https://github.com/VortexBypass/AFK-.lol-Bypass/issues
-// @license      MIT
+// @require      https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/main/AFKdotlol.yes.js
 // @downloadURL  https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/main/AFKdotlol.user.js
 // @updateURL    https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/main/AFKdotlol.meta.js
 // ==/UserScript==
