81d31384a6f6eb35058d5cd97eda247875141403
VortexBypass <taykay1056@gmail.com>
2025-11-12 17:04:14 -0400

    Update AFKdotlol.meta.js

--- AFKdotlol.meta.js
+++ AFKdotlol.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         AFK™.lol Bypasser Meta
-// @version      1.0.5
+// @version      1.1.5
 // @updateURL    https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/refs/heads/main/AFKdotlol.meta.js
 // @downloadURL  https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/refs/heads/main/AFKdotlol.user.js
 // ==/UserScript==
