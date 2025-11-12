3337e8e40123b10dd27d54f93b6e15ad82fd6bd0
VortexBypass <taykay1056@gmail.com>
2025-11-12 17:00:06 -0400

    Update AFKdotlol.meta.js

--- AFKdotlol.meta.js
+++ AFKdotlol.meta.js
@@ -1,20 +1,6 @@
 // ==UserScript==
-// @name         AFK™.lol Bypass
-// @namespace    https://discord.gg/sideloading
-// @version      1.1.5
-// @description  Bypass rekonise bstlar and mboost.me with Volcano/Work.ink support
-// @author       AFK™.lol
-// @match        https://bstlar.com/*
-// @match        https://rekonise.com/*
-// @match        https://mboost.me/*
-// @match        https://linkvertise.com/*
-// @match        https://link-unlock.com/*
-// @match        https://key.volcano.wtf/*
-// @match        https://work.ink/*
-// @icon         https://i.ibb.co/KcXkWgGm/B57-FBD3-E-489-E-4-F0-D-A5-C0-08017-DA44-C4-E.png
-// @grant        unsafeWindow
-// @run-at       document-start
-// @require      https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/main/AFKdotlol.yes.js
-// @downloadURL  https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/main/AFKdotlol.user.js
-// @updateURL    https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/main/AFKdotlol.meta.js
+// @name         AFK™.lol Bypasser Meta
+// @version      1.0.5
+// @updateURL    https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/refs/heads/main/AFKdotlol.meta.js
+// @downloadURL  https://raw.githubusercontent.com/VortexBypass/AFK-.lol-Bypass/refs/heads/main/AFKdotlol.user.js
 // ==/UserScript==
